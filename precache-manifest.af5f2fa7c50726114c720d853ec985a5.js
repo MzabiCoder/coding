98e7547ceca427e50b0969855d3be358ee092521
Nabil <mzabi30@gmail.com>
2021-02-21 23:52:11 -0800

    Updates

---
+++ precache-manifest.af5f2fa7c50726114c720d853ec985a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eab25636d3cf421eefa19cbb11e5b51",
+    "url": "/coding/index.html"
+  },
+  {
+    "revision": "f09b1aba5cd5c8ac2645",
+    "url": "/coding/static/css/main.db48d610.chunk.css"
+  },
+  {
+    "revision": "36fb1effb72e5aaab8af",
+    "url": "/coding/static/js/2.cc63fdd5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/coding/static/js/2.cc63fdd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09b1aba5cd5c8ac2645",
+    "url": "/coding/static/js/main.79e3588b.chunk.js"
+  },
+  {
+    "revision": "33eef2ddd39ac4832c2a",
+    "url": "/coding/static/js/runtime-main.bede05dd.js"
+  },
+  {
+    "revision": "1542c1188204879edee18cf6e115d934",
+    "url": "/coding/static/media/20-minutes-to-toned-thumb.1542c118.jpg"
+  },
+  {
+    "revision": "003901761c4b2f91b5c1b144747d4e83",
+    "url": "/coding/static/media/charles-race-thumb.00390176.jpg"
+  },
+  {
+    "revision": "578dacbc0d22c3d6ff585347dadb0e73",
+    "url": "/coding/static/media/full-body-hiit-thumb.578dacbc.jpg"
+  },
+  {
+    "revision": "a55f946a95a88b198f5fb3f1173a95f6",
+    "url": "/coding/static/media/kafue-river-thumb.a55f946a.jpg"
+  },
+  {
+    "revision": "e0a7c9061a0e72bed419a8b9eaf2b286",
+    "url": "/coding/static/media/lake-inniscarra-thumb.e0a7c906.jpg"
+  },
+  {
+    "revision": "3e82c42342a27af7cce1e89930288333",
+    "url": "/coding/static/media/performance-series-thumb.3e82c423.jpg"
+  },
+  {
+    "revision": "04edd5427547b03287f6d75cfabea837",
+    "url": "/coding/static/media/shred-and-burn-thumb.04edd542.jpg"
+  },
+  {
+    "revision": "7cae9ed133157efe82011594ed819bcc",
+    "url": "/coding/static/media/slow-pulls-thumb.7cae9ed1.jpg"
+  }
+]);
